a942774d6a55d92a8ef47bc63f11646d10dc105a
0lgaP1 <volhapatapchuk@gmail.com>
2024-09-27 15:51:36 +0200

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Button} from "./components/button/Button";
 
 // import { MouseEvent } from 'react';
 
 function App() {
-    const onClickHandler = (name: string) => {
-        console.log(name)
+    const onClickHandler = (name: string) => {console.log(name)
     }
     const foo1 = () => {
         console.log(100200)
@@ -25,27 +24,29 @@ function App() {
         console.log("Stupid button")
     }
 
+    let [a, setA] = useState<number>(0)
+    const onClickHandlerForUseState = () => {
+        setA(++a);
+        console.log(a)
+    }
+
     return (
         <>
             {/*<button onClick={(event:MouseEvent<HTMLButtonElement>)=>{onClickHandler('Jacob')}}>The name of the 1st subscriber</button>*/}
             <button onClick={() => {
                 onClickHandler('Jacob')
-            }}>The name of the 1st subscriber
-            </button>
-            <button onClick={() => {
-                onClickHandler('Jack')
-            }}>The name of the 2nd subscriber
-            </button>
-
+            }}>The name of the 1st subscriber </button>
+            <button onClick={() => {onClickHandler('Jack')}}>The name of the 2nd subscriber</button>
             <button onClick={foo1}>1</button>
-            <button onClick={() => {
-                foo2(100200)
-            }}>2
+            <button onClick={() => {foo2(100200)}}>2
             </button>
             <Button name={"I'm a stupid button"} callBack={componentButton3Handler}/>
             <Button name={"ComponentButton1"} callBack={() => componentButton1Handler('Subscriber Ivan', 22, 'Minsk')}/>
             <Button name={"ComponentButton2"} callBack={() => componentButton2Handler('Subscriber Vasylij')}/>
-        </>
+
+            <h1>{a}</h1>
+            <Button name={"useState Button"} callBack={onClickHandlerForUseState} />
+            </>
     );
 }
 
